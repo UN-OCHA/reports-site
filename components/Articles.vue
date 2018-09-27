fabd3b19c156f62c9ce776200bc9d7ad59accc4b
Chris Ruppel <chris@humanitarianresponse.info>
2018-09-27 14:39:53 +0200

    feat(articles): display Article images when present

--- components/Articles.vue
+++ components/Articles.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <article :key="article.sys.id" v-for="article in articles"  class="card card--article">
-      <h3 class="card__title">{{ article.fields.title }}</h3>
-      <div class="md" v-html="$md.render(article.fields.article)"></div>
+    <article :key="article.sys.id" v-for="article in articles"  class="card card--article article">
+      <span class="card__title">{{ article.fields.sectionHeading }}</span>
+      <div class="article__content" v-bind:class="{ 'article__content--has-image': article.fields.image }">
+        <div class="article__text md">
+          <h3>{{ article.fields.title }}</h3>
+          <div v-html="$md.render(article.fields.article)"></div>
+        </div>
+        <img class="article__image" v-if="article.fields.image" :src="article.fields.image.fields.file.url" :alt="article.fields.image.fields.description">
+      </div>
     </article>
   </div>
 </template>
@@ -13,4 +19,46 @@
   }
 </script>
 
-<style scoped></style>
+<style scoped>
+  .card__title {
+    display: block;
+    margin-bottom: 1rem;
+  }
+
+  @media (min-width: 900px) {
+    .article__text {
+      float: left;
+      clear: left;
+      width: calc(66.666% - 1rem);
+    }
+
+    .article__image {
+      float: left;
+      width: 33.333%;
+    }
+
+    @supports (display: grid) {
+      .article__content--has-image {
+        display: grid;
+        grid-template-areas: 'articleText articleImages';
+        grid-template-rows: 1fr;
+        grid-template-columns: 2fr 1fr;
+        grid-gap: 1rem;
+      }
+
+      .article__text,
+      .article__image {
+        float: none;
+        width: auto;
+      }
+
+      .article__text {
+        grid-area: articleText;
+      }
+      .article__image {
+        grid-area: articleImages;
+        width: 100%;
+      }
+    }
+  }
+</style>
