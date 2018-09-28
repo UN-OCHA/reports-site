3e13e86684ec1401fe8d2eda0d9382ff62bfe1ad
Chris Ruppel <chris@humanitarianresponse.info>
2018-09-28 16:32:31 +0200

    fix(keyMessages): adjust grid to conserve vertical space

--- components/KeyFigures.vue
+++ components/KeyFigures.vue
@@ -26,7 +26,7 @@
   @supports (display: grid) {
     .figures {
       display: grid;
-      grid-template-rows: 1fr 1fr;
+      /*grid-template-rows: 1fr 1fr;*/
       grid-template-columns: 1fr 1fr;
       grid-gap: 1rem;
     }
