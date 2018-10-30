c892e79cb7f45668daee0f32b8f18dd3e3d936f1
Chris Ruppel <chris@humanitarianresponse.info>
2018-10-30 11:38:41 +0100

    DSR-8: set keyFinancialsManual.caption to be title in Contentful UI

--- _migrations/01-create-keyFinancialsManual.js
+++ _migrations/01-create-keyFinancialsManual.js
@@ -1,5 +1,9 @@
 module.exports = function(migration) {
+  //
+  // ContentType: keyFinancialsManual
+  //
   const keyFinancialsManual = migration.createContentType('keyFinancialsManual')
+
   keyFinancialsManual
     .name('Key Financials (manual)')
     .description('Manually-entered Key Financial data.')
@@ -8,24 +12,29 @@ module.exports = function(migration) {
     .createField('financial')
     .name('Financial figure')
     .type('Symbol')
+    .required(true)
     .validations([
       { 'size': {max: 10} }
     ])
-    .required(true)
 
   keyFinancialsManual
     .createField('caption')
     .name('Caption')
     .type('Symbol')
+    .required(true)
     .validations([
       { 'size': {max: 32} }
     ])
-    .required(true)
 
+  // Sets caption as Title in Contentful UI.
   keyFinancialsManual
     .displayField('caption')
 
+  //
+  // Field: SitRep uses keyFinancialsManual (1-to-many)
+  //
   const sitrep = migration.editContentType('sitrep')
+
   sitrep.createField('keyFinancialsManual', {
     items: {
       type: 'Link',
