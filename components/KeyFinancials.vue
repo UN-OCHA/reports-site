61fcc5c47493992df8ed83eaed7dc92f02f109cc
Chris Ruppel <chris@humanitarianresponse.info>
2018-11-14 12:06:57 +0100

    DSR-65: first-pass using client-side mounted() event

--- components/KeyFinancials.vue
+++ components/KeyFinancials.vue
@@ -3,13 +3,13 @@
     <CardHeader />
     <h2 class="card__title">Key Financials</h2>
     <div class="figures clearfix">
-      <div v-if="!content" class="figures-none">
-        No Financial data to display.
-      </div>
-      <figure v-else v-for="figure in content" :key="figure.sys.id">
-        <span class="data">{{ figure.fields.financial }}</span>
+      <figure v-if="ftsData" v-for="figure in ftsData" :key="figure.sys.id">
+        <span class="data">{{ figure.fields.figure }}</span>
         <figcaption>{{ figure.fields.caption }}</figcaption>
       </figure>
+      <div v-else class="figures-none">
+        Financial data could not be found.
+      </div>
     </div>
     <a v-if="!!ftsUrl" :href="ftsUrl" target="_blank" class="fts-url">FTS</a>
     <CardActions :frag="'#' + this.cssId" />
@@ -18,6 +18,7 @@
 </template>
 
 <script>
+  import axios from 'axios';
   import Card from './Card.vue';
   import KeyFigures from './KeyFigures.vue';
 
@@ -27,6 +28,13 @@
     props: {
       'content': Array,
       'ftsUrl': String,
+      'ftsPlanId': Number,
+    },
+
+    data() {
+      return {
+        ftsData: [],
+      }
     },
 
     computed: {
@@ -37,8 +45,88 @@
         else {
           return 'cf-keyFinancials-notAvailable';
         }
-      }
-    }
+      },
+    },
+
+    // Nuxt uses this to make async API calls to Contentful during SSR.
+    mounted() {
+      return Promise.all([
+        axios({
+          url: '/data/v2-flow-plan-overview-progress-2018.json',
+          method: 'GET',
+        })
+      ])
+      .then(([response]) => this.formatFTSData(response))
+      .catch(console.error)
+    },
+
+    methods: {
+      formatFTSData(response) {
+        const plan = response.data.data.plans.filter(plan => plan.id === 639)[0];
+
+        // The structure mimics Contentful JSON API so that our template above
+        // doesn't have to be duplicated based on input data.
+        this.ftsData = [
+          {
+            sys: {
+              id: `${639}-requirements.revisedRequirements`,
+            },
+            fields: {
+              figure: '$' + this.formatNumber(plan.requirements.revisedRequirements),
+              caption: 'requirements.revisedRequirements',
+            },
+          },
+          {
+            sys: {
+              id: `${639}-funding.totalFunding`,
+            },
+            fields: {
+              figure: '$' + this.formatNumber(plan.funding.totalFunding),
+              caption: 'funding.totalFunding',
+            },
+          },
+          {
+            sys: {
+              id: `${639}-funding.progress`,
+            },
+            fields: {
+              figure: Math.round(plan.funding.progress) + '%',
+              caption: 'funding.progress',
+            },
+          },
+        ];
+      },
+
+      formatNumber(n) {
+        const ranges = [
+          { divider: 1e12 , suffix: 'T' },
+          { divider: 1e9 , suffix: 'B' },
+          { divider: 1e6 , suffix: 'M' },
+          { divider: 1e3 , suffix: 'K' }
+        ];
+
+        function doFormatting(n) {
+          // if negative, add a minus and format the positive number.
+          if (n < 0) {
+            return '-' + doFormatting(-n);
+          }
+
+          // Determine which unit we're attaching
+          for (var i = 0; i < ranges.length; i++) {
+            if (n >= ranges[i].divider) {
+              // the + is a coercion to String, which allows us to use toFixed()
+              // but it will skip the decimal when equal to 0
+              return +(n / ranges[i].divider).toFixed(1) + ranges[i].suffix;
+            }
+          }
+
+          return n;
+        }
+
+        return doFormatting(n);
+      },
+    },
+
   }
 </script>
 
