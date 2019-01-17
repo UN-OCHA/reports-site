74102bbecfaca0b622bb6955100ca83eee267bea
Chris Ruppel <chris@humanitarianresponse.info>
2019-01-17 09:37:15 -0500

    OPS-5309: configure app to use Preview mode when env vars are present

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -3,9 +3,10 @@ const contentful = require('contentful');
 // use default environment config for convenience
 // these will be set via `env` property in nuxt.config.js
 const config = {
+  host: process.env.CTF_HOST || 'cdn.contentful.com',
   space: process.env.CTF_SPACE_ID,
   environment: process.env.CTF_ENVIRONMENT || 'master',
-  accessToken: process.env.CTF_CDA_ACCESS_TOKEN
+  accessToken: process.env.CTF_CDA_PREVIEW_TOKEN || process.env.CTF_CDA_ACCESS_TOKEN,
 }
 
 // export `createClient` to use it in page components
