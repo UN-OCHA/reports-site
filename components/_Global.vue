14abf37484e4bda96900d81cdf81f8f929816224
Chris Ruppel <chris@humanitarianresponse.info>
2019-02-01 17:06:47 -0600

    DSR-97: comments for _Global#getImageHeight

--- components/_Global.vue
+++ components/_Global.vue
@@ -71,18 +71,25 @@
     },
 
     methods: {
+      //
       // This handler intentionally left blank in order to facilitate click
       // handling outside the AppHeader > Share component. It uses vue-clickaway
       // but iPhone does not delegate clicks so we set up a blank handler on
       // the whole page. It's stupid.
       //
       // @see https://www.quirksmode.org/blog/archives/2010/09/click_event_del.html
+      //
       noop() {},
 
+      //
       // Input any width value to get the height as defined by the proportions.
       // of the image stored in Contentful for this Entry.
       //
-      // We round to the nearest pixel for you, just plop it in your template.
+      // referenceImage must be the full Contentful JSON response for the asset
+      // field you want to use as a ratio calculation
+      //
+      // We round the return value to to the nearest pixel.
+      //
       getImageHeight(requestedWidth, referenceImage) {
         const width = referenceImage.fields.file.details.image.width;
         const height = referenceImage.fields.file.details.image.height;
