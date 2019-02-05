cd7b2e122c19582d61aef09c99c0b97866e24237
Chris Ruppel <chris@humanitarianresponse.info>
2019-02-05 16:58:54 -0600

    DSR-144: implement pdfHeader on PDF Snaps

--- components/SnapPage.vue
+++ components/SnapPage.vue
@@ -37,12 +37,81 @@
         // @see middleware/i18n.js
         const cookies = document.cookie;
 
-        return `${this.snapEndpoint}?url=${encodeURIComponent(this.sitRepUrl)}&service=${this.requestingService}&output=pdf&media=print&logo=ocha&headerTitle=${encodeURIComponent(this.title.toUpperCase())}&headerSubtitle=${encodeURIComponent(this.subtitle)}&headerDescription=${encodeURIComponent(this.description)}&footerText=${encodeURIComponent(this.sitRepUrl)}&cookies=${encodeURIComponent(cookies)}&pdfFooter=${encodeURIComponent(this.pdfFooter)}`;
+        return `${this.snapEndpoint}?url=${encodeURIComponent(this.sitRepUrl)}&service=${this.requestingService}&output=pdf&media=print&logo=ocha&headerTitle=${encodeURIComponent(this.title.toUpperCase())}&headerSubtitle=${encodeURIComponent(this.subtitle)}&headerDescription=${encodeURIComponent(this.description)}&footerText=${encodeURIComponent(this.sitRepUrl)}&cookies=${encodeURIComponent(cookies)}&pdfHeader=${encodeURIComponent(this.pdfHeader)}&pdfFooter=${encodeURIComponent(this.pdfFooter)}`;
       },
 
       filename() {
         const dateUpdated = this.$moment(this.$store.state.reportMeta.dateUpdated).locale(this.locale).format('DD MMM YYYY');
         return `${this.$t('Situation Report', this.locale)} - ${this.$store.state.reportMeta.title} - ${dateUpdated}.${this.output}`;
+      },
+
+      pdfHeader() {
+        return `
+          <header class="pdf-header">
+            <div class="pdf-header__meta">
+              <div class="pdf-header__title">${this.title}</div>
+              <div class="pdf-header__subtitle">${this.subtitle}</div>
+              <div class="pdf-header__description">${this.description}</div>
+            </div>
+            <div class="pdf-header__logo-wrapper">
+              <img src="__LOGO_SRC__" alt="logo" class="pdf-header__logo">
+            </div>
+          </header>
+          <style type="text/css">
+            *,
+            *:before,
+            *:after {
+              box-sizing: border-box;
+              -webkit-print-color-adjust: exact;
+            }
+
+            .pdf-header {
+              width: 100%;
+              margin: 2.5mm 7.5mm 7.5mm;
+              padding-bottom: 10px;
+              border-bottom: 2px solid #4c8cca;
+
+              font-family: "Roboto Condensed", Roboto, serif;
+              font-weight: 400;
+              font-size: 12px;
+              white-space: nowrap;
+
+              display: grid;
+              grid-template-areas: "logo meta";
+              grid-template-columns: __LOGO_WIDTH__px 2fr;
+            }
+
+            .pdf-header__meta {
+              grid-area: meta;
+              font-size: inherit;
+              padding-left: 10px;
+              margin-left: 10px;
+              border-left: 1px solid #4c8cca;
+              color: #4c8cca;
+            }
+            .pdf-header__title {
+              line-height: .9;
+              font-size: 22px;
+              font-weight: 700;
+              text-transform: uppercase;
+            }
+            .pdf-header__subtitle {
+              font-size: 15px;
+            }
+            .pdf-header__description {
+              font-style: italic;
+            }
+
+            .pdf-header__logo-wrapper {
+              grid-area: logo;
+            }
+            .pdf-header__logo {
+              width: __LOGO_WIDTH__px;
+              height: __LOGO_HEIGHT__px;
+              position: relative;
+              top: 2px;
+            }
+          </style>`;
       },
 
       pdfFooter() {
