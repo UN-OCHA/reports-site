a5c294de5bee7e8e7c7e6e3cf751f4f29e0596f7
Chris Ruppel <chris@humanitarianresponse.info>
2019-04-04 18:23:55 +0200

    DSR-197: use moment.utc() to instantiate funding dates

--- components/KeyFinancials.vue
+++ components/KeyFinancials.vue
@@ -111,10 +111,7 @@
       ftsDataYear() {
         const plan = this.content && this.content.filter(plan => plan.id === this.ftsPlanId)[0] || false;
 
-        // Using moment caused timezone confusions and installing `tz()` just to
-        // run it one time seemed heavy-handed for what we want. Instead we're
-        // taking the year out of `startDate` and slicing it out of the string.
-        return plan && plan.startDate.substring(0, 4);
+        return plan && this.$moment.utc(plan.startDate).locale(this.locale).format('YYYY');
       },
     },
 
