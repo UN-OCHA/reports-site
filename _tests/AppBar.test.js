8544655230599c906920c0744a2b81fc4abbfd62
Chris Ruppel <chris@humanitarianresponse.info>
2019-04-05 17:32:12 +0200

    DSR-167: note about AppBar SitRep test

--- _tests/AppBar.test.js
+++ _tests/AppBar.test.js
@@ -10,6 +10,10 @@ describe('AppBar', () => {
     await expect(className).toMatch('is--expanded');
   });
 
+  // TODO: wait for the AppBar.mounted() event before running this test.
+  //
+  // Since the list is populated on nthe client, there's a race condition unless
+  // we wait for CTF to return the content.
   it('should contain the same SitRep list as the homepage', async () => {
     const homeListLength = await page.$$eval('main .card--sitreps .sitrep-group', el => el.length);
     const appBarListLength = await page.$$eval('.app-bar .sitrep-group', el => el.length);
