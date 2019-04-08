23e3e75b9673feb4fa72757bd2d6f22a89e4c4a1
Chris Ruppel <chris@humanitarianresponse.info>
2019-04-08 09:06:32 +0200

    DSR-167: fix race condition on client-side AppBar content

--- _tests/AppBar.test.js
+++ _tests/AppBar.test.js
@@ -10,13 +10,14 @@ describe('AppBar', () => {
     await expect(className).toMatch('is--expanded');
   });
 
-  // TODO: wait for the AppBar.mounted() event before running this test.
-  //
-  // Since the list is populated on nthe client, there's a race condition unless
-  // we wait for CTF to return the content.
   it('should contain the same SitRep list as the homepage', async () => {
     const homeListLength = await page.$$eval('main .card--sitreps .sitrep-group', el => el.length);
-    const appBarListLength = await page.$$eval('.app-bar .sitrep-group', el => el.length);
-    await expect(homeListLength).toBe(appBarListLength);
+
+    // Since the AppBar loads SitReps on client-side only, we have to wait for
+    // the selector to exist before evaluating.
+    await page.waitForSelector('.app-bar .sitrep-group', {timeout: 10000}).then(async () => {
+      const appBarListLength = await page.$$eval('.app-bar .sitrep-group', el => el.length);
+      await expect(homeListLength).toBe(appBarListLength);
+    });
   });
 });
