43cffb209393569cf0a367eb59e196e93599b358
Chris Ruppel <chris@humanitarianresponse.info>
2019-04-08 17:12:13 +0200

    DSR-167: test - FTS box

---
+++ _tests/Funding.test.js
@@ -0,0 +1,28 @@
+describe('Funding', () => {
+  beforeAll(async () => {
+    await page.setJavaScriptEnabled(false);
+    await page.goto('https://reports.unocha.org/fr/country/burundi/');
+  });
+
+  // If the element is either missing (due to the "Funding not available") we
+  // will catch and return -2, which is always less than -1.
+  //
+  // 0% progress, while not ideal, is a valid result. Must be greater than -1.
+  it('should display FTS figures with JS disabled', async () => {
+    const progress = await page.$eval('.figure--progress .data', el => Number(el.innerText.replace('%',''))).catch(err => -2);
+    await expect(progress).toBeGreaterThan(-1);
+  });
+});
+
+describe('Funding', () => {
+  beforeAll(async () => {
+    await page.setJavaScriptEnabled(true);
+    await page.goto('https://reports.unocha.org/fr/country/burundi/', {waitUntil: 'networkidle0'});
+  });
+
+  // Same here with -2
+  it('should display FTS figures with JS enabled', async () => {
+    const progress = await page.$eval('.figure--progress .data', el => Number(el.innerText.replace('%',''))).catch(err => -2);
+    await expect(progress).toBeGreaterThan(-1);
+  });
+});
