f63e46bedd12a6545a162e596b517ad8c72c68ef
Chris Ruppel <chris@humanitarianresponse.info>
2019-06-03 15:06:56 +0200

    DSR-237: E2E - AboutPage

---
+++ _tests/About.test.js
@@ -0,0 +1,39 @@
+const en = require('../locales/en.js');
+const fr = require('../locales/fr.js');
+
+describe('AboutPage', () => {
+  beforeAll(async () => {
+    await page.goto('https://reports.unocha.org/en/about/');
+  });
+
+  it('/en/about/ should contain a localized heading "About this Site"', async () => {
+    const expectedText = en['About this site'].toUpperCase();
+    const actualText = await page.$eval('.card--content .card__title', el => el.innerText);
+    expect(actualText).toBe(expectedText);
+  });
+});
+
+describe('AboutPage', () => {
+  beforeAll(async () => {
+    await page.goto('https://reports.unocha.org/fr/about/');
+  });
+
+  it('/fr/about/ should contain a localized heading "About this Site"', async () => {
+    const expectedText = fr['About this site'].toUpperCase();
+    const actualText = await page.$eval('.card--content .card__title', el => el.innerText);
+    expect(actualText).toBe(expectedText);
+  });
+});
+
+
+describe('AboutPage', () => {
+  beforeAll(async () => {
+    await page.goto('https://reports.unocha.org/en/about/');
+  });
+
+  it('should contain the Contentful graphic in the sidebar', async () => {
+    const expectedAltText = 'Contentful';
+    const actualAltText = await page.$eval('.card--sidebar img', el => el.alt);
+    expect(actualAltText).toContain(expectedAltText);
+  });
+});
