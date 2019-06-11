f0949344307c2ce4174fbfb92feaa9b5e429fe80
Chris Ruppel <chris@humanitarianresponse.info>
2019-06-11 15:11:54 +0200

    DSR-238: update E2E to reflect new error page structure
    Also switch the EN-Burundi to AR-Burundi because they have
    published an English SitRep.

--- _tests/404.test.js
+++ _tests/404.test.js
@@ -2,6 +2,9 @@ const env = {
   baseUrl: 'https://reports.unocha.org',
 };
 
+const ar = require('../locales/ar.js');
+
+
 //
 // For these tests we are inspecting actual page content rather than relying on
 // the HTTP response code itself. The reason is because in DSR-173 we actually
@@ -14,69 +17,70 @@ const env = {
 // being shown to the user.
 //
 describe('404', () => {
-  // This is the expected title for any 404.
-  const expectedTitle = 'PAGE NOT FOUND';
+  // Set up some expected titles for various languages.
+  const expectedTitleEn = 'PAGE NOT FOUND';
+  const expectedTitleAr = ar['Page not found'];
 
   // Invalid language
   it('should display 404 page at /zz', async () => {
     await page.goto(`${env.baseUrl}/zz`);
-    await page.waitFor('.card--404').then(async () => {
-      const actualTitle = await page.$eval('.card--404 .card__title', el => el.innerText);
-      await expect(actualTitle).toBe(expectedTitle);
+    await page.waitFor('.card--error').then(async () => {
+      const actualTitle = await page.$eval('.card--error .card__title', el => el.innerText);
+      await expect(actualTitle).toBe(expectedTitleEn);
     });
   });
 
   // Invalid language
   it('should display 404 page at /show-me-404', async () => {
     await page.goto(`${env.baseUrl}/show-me-404`);
-    await page.waitFor('.card--404').then(async () => {
-      const actualTitle = await page.$eval('.card--404 .card__title', el => el.innerText);
-      await expect(actualTitle).toBe(expectedTitle);
+    await page.waitFor('.card--error').then(async () => {
+      const actualTitle = await page.$eval('.card--error .card__title', el => el.innerText);
+      await expect(actualTitle).toBe(expectedTitleEn);
     });
   });
 
   // Valid language, invalid page slug
   it('should display 404 page at /en/show-me-404', async () => {
     await page.goto(`${env.baseUrl}/en/show-me-404`);
-    await page.waitFor('.card--404').then(async () => {
-      const actualTitle = await page.$eval('.card--404 .card__title', el => el.innerText);
-      await expect(actualTitle).toBe(expectedTitle);
+    await page.waitFor('.card--error').then(async () => {
+      const actualTitle = await page.$eval('.card--error .card__title', el => el.innerText);
+      await expect(actualTitle).toBe(expectedTitleEn);
     });
   });
 
   // Invalid language, invalid page slug
   it('should display 404 page at /zz/show-me-404', async () => {
     await page.goto(`${env.baseUrl}/zz/show-me-404`);
-    await page.waitFor('.card--404').then(async () => {
-      const actualTitle = await page.$eval('.card--404 .card__title', el => el.innerText);
-      await expect(actualTitle).toBe(expectedTitle);
+    await page.waitFor('.card--error').then(async () => {
+      const actualTitle = await page.$eval('.card--error .card__title', el => el.innerText);
+      await expect(actualTitle).toBe(expectedTitleEn);
     });
   });
 
   // Valid language, non-existent SitRep
-  it('should display 404 page at /en/country/burundi/', async () => {
-    await page.goto(`${env.baseUrl}/en/country/burundi/`);
-    await page.waitFor('.card--404').then(async () => {
-      const actualTitle = await page.$eval('.card--404 .card__title', el => el.innerText);
-      await expect(actualTitle).toBe(expectedTitle);
+  it('should display 404 page at /ar/country/burundi/', async () => {
+    await page.goto(`${env.baseUrl}/ar/country/burundi/`);
+    await page.waitFor('.card--error').then(async () => {
+      const actualTitle = await page.$eval('.card--error .card__title', el => el.innerText);
+      await expect(actualTitle).toBe(expectedTitleAr);
     });
   });
 
   // Invalid language, non-existent SitRep
   it('should display 404 page at /zz/country/burundi/', async () => {
     await page.goto(`${env.baseUrl}/zz/country/burundi/`);
-    await page.waitFor('.card--404').then(async () => {
-      const actualTitle = await page.$eval('.card--404 .card__title', el => el.innerText);
-      await expect(actualTitle).toBe(expectedTitle);
+    await page.waitFor('.card--error').then(async () => {
+      const actualTitle = await page.$eval('.card--error .card__title', el => el.innerText);
+      await expect(actualTitle).toBe(expectedTitleEn);
     });
   });
 
   // Valid language, invalid static URL parameters
   it('should display 404 page at /en/invalid/url/', async () => {
     await page.goto(`${env.baseUrl}/en/invalid/url/`);
-    await page.waitFor('.card--404').then(async () => {
-      const actualTitle = await page.$eval('.card--404 .card__title', el => el.innerText);
-      await expect(actualTitle).toBe(expectedTitle);
+    await page.waitFor('.card--error').then(async () => {
+      const actualTitle = await page.$eval('.card--error .card__title', el => el.innerText);
+      await expect(actualTitle).toBe(expectedTitleEn);
     });
   });
 });
