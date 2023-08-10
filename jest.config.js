d8c2df486b0e5ab8db02996256d7d8f2e2db839e
Andy Footner <andyfootner@netscape.net>
2023-08-10 15:06:56 +0200

    chore: replace vue-jest and update more packages
    Refs: updates

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ const config = {
   moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
+    '.*\\.(vue)$': '@vue/vue3-jest',
   },
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
